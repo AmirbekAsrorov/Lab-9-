6a6309236458364c330821d181f9b28bd2e9632b
AmirbekAsrorov <147371906+AmirbekAsrorov@users.noreply.github.com>
2024-05-03 20:06:08 +0500

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+     int a[6][6]={
+     {0,12,0,0,0,25},{12,0,13,15,0,0},{0,13,0,0,5,2},{0,15,0,0,5,13},{0,0,5,5,0,0},{25,0,2,13,0,0}
+     };
+int n;
+cin>>n;
+    int sum=0;
+    for(auto k: a[n]) {
+        sum+=k;
+    }
+    cout<<sum<<"\n";
+    return 0;
+}
