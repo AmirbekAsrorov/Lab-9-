cbd059b71e0e4923a44cee56c3521448c97319dd
AmirbekAsrorov <147371906+AmirbekAsrorov@users.noreply.github.com>
2024-05-03 19:35:35 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -1,51 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <climits>
-
-
 using namespace std;
 
 
-vector<int> findSeparationLevels(const vector<vector<int>>& graph, int source){
-    int n=graph.size();
-    vector<int> levels(n, -1);
-    vector<bool> visited(n, false);
-    queue<int>q;
-    visited[source]=true;
-    levels[source]=0;
-    q.push(source);
+const int INF = -1;
+
+void findSeparationLevels(int N, const vector<vector<int>>& graph, vector<int>& levels){
+    queue<int> q;
+    vector<bool> visited(N, false);
+    q.push(0);
+    levels[0]=0;
+    visited[0]=true;
     while(!q.empty()){
         int u=q.front();
         q.pop();
-        for(int v : graph[u]){
-            if(!visited[v]){
-                visited[v]=true;
+        for (int v : graph[u]){
+            if (!visited[v]){
                 levels[v]=levels[u]+1;
+                visited[v]=true;
                 q.push(v);
             }
         }
     }
-    return levels;
 }
 int main(){
-    int n, m;
-    cout<<"Enter the number of users (nodes) in the network: ";
-    cin>>n;
-    cout<<"Enter the number of friendships (edges) in the network: ";
-    cin>>m;
-    vector<vector<int>> graph(n);
-    cout<<"Enter "<<m<<" friendships (pairs of users):\n";
-    for(int i=0;i<m;++i){
-        int u,v;
+    int N, M;
+    cin>>N>>M;
+    vector<vector<int>> graph(N);
+    vector<int> levels(N, INF);
+    for(int i = 0; i < M; ++i){
+        int u, v;
         cin>>u>>v;
         graph[u].push_back(v);
         graph[v].push_back(u);
     }
-    vector<int> separationLevels=findSeparationLevels(graph, 0);
-    cout<<"Separation levels from source user (user 0):\n";
-    for(int i = 0; i < n; ++i){
-        cout<<separationLevels[i]<<endl;
+    findSeparationLevels(N, graph, levels);
+    for(int i = 0; i < N; ++i){
+        cout<<levels[i]<<" ";
     }
+    cout<<endl;
     return 0;
 }
